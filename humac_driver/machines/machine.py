80f76a3b77b0d2ae32b7522f9feeaf307b7ab066
raviraj@parentheses.systems <raviraj@parentheses.systems>
2025-12-12 12:12:04 +0530

    some chnages in machine object

--- humac_driver/machines/machine.py
+++ humac_driver/machines/machine.py
@@ -31,6 +31,7 @@ class Machine(mp.Process):
             while True:
                 result = self.driver.poll(handle)
                 logging.info(result)
+                time.sleep(1)
         except Exception or  KeyboardInterrupt as e :
             logging.info(f"[PID {pid}] Connection failed {self.edgeid}: {e}")
         self.driver.disconnect()
