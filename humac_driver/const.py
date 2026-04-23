bc3543687c6ec92b5fe8fcdd1a10daa00c385efd
RaviTop <92226554+RaviTop@users.noreply.github.com>
2026-04-23 11:48:09 +0530

    update code

--- humac_driver/const.py
+++ humac_driver/const.py
@@ -13,11 +13,11 @@ MQTT_PORT = 1883
 TOPIC_PRO = "pspl-iot/telemetry_cnc/programe"
 TOPIC_BLK = "pspl-iot/telemetry_cnc/block"
 MQTT_PASS = None
-MQTT_CLI = "PICNC02"
+MQTT_CLI = "PICNC01"
 
 config = {
         "machines":[
-           {"fanuc":{"ip":"193.168.10.2", "port":8193,"timeout":5,"edgid" :"ed4200013" , "machineid":"PICNC02"}},
+           {"fanuc":{"ip":"193.168.10.2", "port":8193,"timeout":5,"edgid" :"ed4200012" , "machineid":"PICNC01"}},
           #  {"hass":{"ip":"192.168.0.2", "port":8082,"timeout":5,"edgid" :"ed4200022" , "machineid":"SACNC02"}},
         ]
     }
