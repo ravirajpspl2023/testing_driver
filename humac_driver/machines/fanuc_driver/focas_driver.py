e9de8527c7c4a906ece07274d56615fe3a48731c
RaviTop <92226554+RaviTop@users.noreply.github.com>
2026-05-10 08:18:03 +0530

    update code

--- humac_driver/machines/fanuc_driver/focas_driver.py
+++ humac_driver/machines/fanuc_driver/focas_driver.py
@@ -462,7 +462,7 @@ class FocasDriver(object):
                 for i in range(ds_info_out.total):
                     filename = ds_file_out[i].file.decode('ascii').strip('\x00')
                     logging.info(f"Starting download for: {filename}")
-
+                    local_path = f"./humac/{filename}"
                     # --- START DOWNLOAD FLOW ---
                     # 2. Start the transfer for this specific file
                     # Format usually requires //DATA_SV/ prefix
@@ -470,35 +470,39 @@ class FocasDriver(object):
                     end_line_path = remote_full_path + b'\x00'
                     logging.info(f"full path : {end_line_path}")
                     buf_path = ctypes.create_string_buffer(end_line_path)  # Null-terminated path
-                    ret_upstart = fwlib.cnc_upstart4(self.handle, 0, b'')
 
-                    err = ODBERR()
-                    fwlib.cnc_getdtailerr(self.handle, ctypes.byref(err))
-                    logging.error(f"Detail Error for {filename}: err_no={err.err_no}, err_dtl={err.err_dtno}")
+                    ret = fwlib.ds_rdfile(self.handle,
+                                          local_path.encode('ascii'),
+                                          filename.encode('ascii'))
+                    logging.info(f"Read file result for {filename}: {ret}")
+                    # ret_upstart = fwlib.cnc_upstart4(self.handle, 0, b'')
 
-                    logging.info(f"Upstart result for {filename}: {ret_upstart}")
+                    # err = ODBERR()
+                    # fwlib.cnc_getdtailerr(self.handle, ctypes.byref(err))
+                    # logging.error(f"Detail Error for {filename}: err_no={err.err_no}, err_dtl={err.err_dtno}")
 
-                    while True:
-                        time.sleep(0.2)
+                    # logging.info(f"Upstart result for {filename}: {ret_upstart}")
 
-                        buf = create_string_buffer(CNC.MAX_BLOCK)
-                        length = c_long(CNC.MAX_BLOCK) 
+                    # while True:
+                    #     time.sleep(0.2)
+                    #     buf = create_string_buffer(CNC.MAX_BLOCK)
+                    #     length = c_long(CNC.MAX_BLOCK) 
 
-                        ret_upload = fwlib.cnc_upload4(self.handle, byref(length), buf)     
-                        logging.info(f"Upload result: {ret_upload}, bytes read: {length.value}")
-                        if ret_upload == 0  and length.value > 0:
-                            block = buf.raw[:length.value].decode('utf-8', errors='ignore').strip('\x00')
-                            logging.info(f"blocks : {block}")
-                        elif ret_upload == -2:
+                    #     ret_upload = fwlib.cnc_upload4(self.handle, byref(length), buf)     
+                    #     logging.info(f"Upload result: {ret_upload}, bytes read: {length.value}")
+                    #     if ret_upload == 0  and length.value > 0:
+                    #         block = buf.raw[:length.value].decode('utf-8', errors='ignore').strip('\x00')
+                    #         logging.info(f"blocks : {block}")
+                    #     elif ret_upload == -2:
                             
-                            break
+                    #         break
 
-                        elif ret_upload != 0 and ret_upload != -2:
-                            logging.error(f"Upload failed with code: {ret_upload}")
-                            break
+                    #     elif ret_upload != 0 and ret_upload != -2:
+                    #         logging.error(f"Upload failed with code: {ret_upload}")
+                    #         break
 
-                    end_ref = fwlib.cnc_upend4(self.handle) 
-                    logging.info(f"Upend result for {filename}: {end_ref}")
+                    # end_ref = fwlib.cnc_upend4(self.handle) 
+                    # logging.info(f"Upend result for {filename}: {end_ref}")
 
                     # full_path = ctypes.create_string_buffer(remote_full_path)
                     # line_no = ctypes.c_ulong(0)
