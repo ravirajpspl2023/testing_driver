3aead37410b7e61ba241152b866c77dabe12d7da
RaviTop <92226554+RaviTop@users.noreply.github.com>
2026-05-16 15:36:11 +0530

    update code

--- humac_driver/machines/fanuc_driver/focas_driver.py
+++ humac_driver/machines/fanuc_driver/focas_driver.py
@@ -475,7 +475,7 @@ class FocasDriver(object):
                         # --- START DOWNLOAD FLOW ---
                         # 2. Start the transfer for this specific file
                         # Format usually requires //DATA_SV/ prefix
-                        remote_full_path = f"//DATA_SV/O1".encode('shift-jis',errors='replace').rstrip(b'\x00')
+                        remote_full_path = f"//DATA_SV/{filename}".encode('shift-jis',errors='replace').rstrip(b'\x00')
                         end_line_path = remote_full_path + b'\x00'
                         #b' //DATA_SV/xw20.nc\x00
                         logging.info(f"full path : {end_line_path}")
