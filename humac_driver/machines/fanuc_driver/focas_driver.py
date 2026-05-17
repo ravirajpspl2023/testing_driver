eda5fea664138eb2f4d5d2f97c1f6f4929d9e1fc
RaviTop <92226554+RaviTop@users.noreply.github.com>
2026-05-17 15:46:55 +0530

    update code

--- humac_driver/machines/fanuc_driver/focas_driver.py
+++ humac_driver/machines/fanuc_driver/focas_driver.py
@@ -108,7 +108,7 @@ class FocasDriver(object):
                 # # Correct path – try without extra '/' or with 'MEMORY/' if DATA_SV fails
                 # name_str = f"//DATA_SV/{CNC.PROGRAME_NAME}"  # or "DATA_SV/lb44.nc" try kara
                 name_bytes = buf.value.rstrip(b'\x00') + b'\x00'
-                path = f"//CNC_MEM/57-6BALL_F_S1.tap".encode('ascii') + b'\x00'
+                path = f"//CNC_MEM/O1".encode('ascii') + b'\x00'
                 logging.info(f"Encoded program path: {path}")
 
                 name_ptr = ctypes.create_string_buffer(path)
