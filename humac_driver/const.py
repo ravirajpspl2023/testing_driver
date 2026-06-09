5b41291ac63e2c0f1a0a3b0f58b7508cbf4c76ad
RaviTop <92226554+RaviTop@users.noreply.github.com>
2026-06-09 16:07:38 +0530

    update code

--- humac_driver/const.py
+++ humac_driver/const.py
@@ -13,7 +13,7 @@ MQTT_PORT = 1883
 TOPIC_PRO = "pspl-iot/telemetry_cnc/programe"
 TOPIC_BLK = "pspl-iot/telemetry_cnc/block"
 MQTT_PASS = None
-MQTT_CLI = "C42"
+MQTT_CLI = "FDPLVMC01"
 
 config = {
         "machines":[
